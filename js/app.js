1c71e5754e86e122d29dc4ec1c13a8c9a3e319b9
LindsayPeltier <lindsaypeltier@gmail.com>
2019-10-13 14:05:39 -0700

    updated table render

--- js/app.js
+++ js/app.js
@@ -1,64 +1,72 @@
 'use strict';
 
-var storeHours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm']
+var storeHours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
 
-function Store(location, minCustHr, maxCustHr, avgCookCust){
+function Store(location, minCustomerHr, maxCustomerHr, avgCookieCust){
   this.location = location;
-  this.minCustHr = minCustHr;
-  this.maxCustHr = maxCustHr;
-  this.avgCookCust = avgCookCust;
+  this.minCustomerHr = minCustomerHr;
+  this.maxCustomerHr = maxCustomerHr;
+  this.avgCookieCust = avgCookieCust;
+  this.estimatedCookiesHr = [];
+  this.totalDailyCookies = [];
   };
  
-Store.prototype.randomCustNumber = function(){
-  return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1) + this.minCustHr);
+Store.prototype.randomCustomerHr = function(){
+  return Math.floor(Math.random() * (this.maxCustomerHr - this.minCustomerHr + 1) + this.minCustomerHr);
   };
 
-Store.prototype.estCookHr = function() {
-  var cookiesPerHour = [];
-  for (var i = 0; i < 14; i++) {
-    cookiesPerHour.push(Math.round(this.randomCustNumber() * this.avgCookCust));
-  }
-  return cookiesPerHour;
+Store.prototype.estimatedCookiesHr = function() {
+  var result = [];
+    for (var i = 0; i < storeHours.length; i++){
+      result.push(Math.floor(this.randomCustomerHr() * this.avgCookieCust));
+    }
+  return result;
+  };
+  
+Store.prototype.totalDailyCookies = function() {
+    var cookieSum = 0;
+    for (var i = 0; i < storeHours.length; i++){
+      cookieSum += this.estimatedCookiesHr()[i];
+    }
+  return cookieSum;
   };
 
-  var Seattle = new Store('Seattle', 23, 65, 6.3);
-  var Tokyo = new Store('Tokyo', 3, 24, 1.2);
-  var Dubai = new Store('Dubai', 11, 38, 3.7);
-  var Paris = new Store('Paris', 20, 38, 2.3);
-  var Lima = new Store('Lima', 2, 16, 4.6);
+var Seattle = new Store('Seattle', 23, 65, 6.3);
+var Tokyo = new Store('Tokyo', 3, 24, 1.2);
+var Dubai = new Store('Dubai', 11, 38, 3.7);
+var Paris = new Store('Paris', 20, 38, 2.3);
+var Lima = new Store('Lima', 2, 16, 4.6);
 
 //table using a loop
-  var table = document.createElement(table); 
-  var tbody = document.createElement(tbody);
-    table.appendChild(tbody);
-  var tfoot = document.createElement(tfoot);
-    table.appendChild(tfoot);
-  var thead = document.createElement(thead);
-    table.appendChild(thead);
+var section = document.getElementById('sales_list');
+var table = document.createElement(table); 
+section.appendChild(table);
+var thead = document.createElement(thead);
+table.appendChild(thead);
+var tbody = document.createElement(tbody);
+thead.appendChild(tbody);
+var tfoot = document.createElement(tfoot);
+tbody.appendChild(tfoot);
 
-  for (var i = 0; i<6;i++){
-    var tr = document.createElement(tr);
-    tbody.appendChild(tr);}
-  
-  for (var j=0; j<17; j++){
-    var tr = document.createElement(td);
-    tr.appendChild(td);}
+for (var i = 0; i<6; i++){
+  var tr = document.createElement(tr);
+  tbody.appendChild(tr);}
 
-  body.appendChild(table);
+for (var j=0; j<17; j++){
+  var tr = document.createElement(td);
+  tr.appendChild(td);}
 
-  document.write('<table>' + + '</table>')
- 
 
 //event handler and listener
 var newStoreForm = document.getElementById('addNewStore');
 newStoreForm.addEventListener('submit',handleSubmit);
 
 function handleSubmit (event){
-  event.preventDefault();
-  var location = event.target.location.value;
-  var mincusthr = event.target.location.value;
-  var maxcusthr = event.target.location.value;
-  var avgcookcust = event.target.location.value;
+event.preventDefault();
+var location = event.target.location.value;
+var mincusthr = event.target.location.value;
+var maxcusthr = event.target.location.value;
+var avgcookcust = event.target.location.value;
 }
 
 var newStoreForm = new (location, minCustHr, maxCustHr, avgCookCust);
